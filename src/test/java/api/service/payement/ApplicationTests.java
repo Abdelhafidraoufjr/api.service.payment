4422776fac348514ad81de33a122e3123040bb94
Abdelhafidraoufjr <raoufabdelhafidjunior@gmail.com>
2024-10-22 15:53:37 +0100

    first-commit

---
+++ src/test/java/api/service/payement/ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.service.payement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
